8cc9a5c1147106f6dd65bdf194b65c14263d3d62
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-10-25 20:41:13 +0600

    data for courses addedd to the server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
+const categories = require('./data/categories.json')
 
 app.get('/', (req, res) => {
     res.send('Educational API is Running');
 });
 
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
 
 
 app.listen(port, () => {
