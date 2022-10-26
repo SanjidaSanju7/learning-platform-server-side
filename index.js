6c751e47ba64eba15ca7c5e6c964d4b86afdc302
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-10-26 12:20:17 +0600

    all courses added to the server

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('Educational API is Running');
@@ -14,6 +15,24 @@ app.get('/', (req, res) => {
 app.get('/course-categories', (req, res) => {
     res.send(categories)
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCategory = categories.filter(category => category.id === id);
+    res.send(selectedCategory)
+})
+
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse)
+})
+
 
 
 app.listen(port, () => {
